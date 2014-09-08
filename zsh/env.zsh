84351d9218fb80b422bd124af368d34271813a18
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2014-09-09 00:06:45 +0300

    zsh: Update env.zsh with OSX conditional
    OSX-related stuff is now conditional.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,20 +1,25 @@
 ####
 ## Environment
-export EDITOR='mvim -p'
+export EDITOR='vim'
+export DOTFILES='~/.dotfiles'
 
-####
-## Android development
-export ANDROIDSDK=/Applications/Android\ Studio.app/sdk
-PATH=$PATH:$ANDROIDSDK/tools
-PATH=$PATH:$ANDROIDSDK/platform-tools
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ####
+    ## Android development
+    export ANDROIDSDK=/Applications/Android\ Studio.app/sdk
+    PATH=$PATH:$ANDROIDSDK/tools
+    PATH=$PATH:$ANDROIDSDK/platform-tools
 
-####
-## Postgres
-export PGDATA=/usr/local/var/postgres
+    ####
+    ## Postgres
+    export PGDATA=/usr/local/var/postgres
 
-####
-## Homebrew
-PATH=$(brew --prefix)/bin:$PATH #prepend so that brew binaries are found first
-PATH=$PATH:~/.dotfiles/brew/extensions
+    ####
+    ## Homebrew
+    PATH=$(brew --prefix)/bin:$PATH #prepend so that brew binaries are found first
+    PATH=$PATH:~/.dotfiles/brew/extensions
+else
+    # Other OSes...
+fi
 
 export PATH
