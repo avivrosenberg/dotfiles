1ba13552e3d2f2e1aa7c070fa03b038f2af65952
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2015-04-18 13:54:14 +0300

    zsh iTerm2 profile plugin: Check for OSX and iTerm2
    Only add the plugin function when running in iTerm2 on OSX.

--- zsh/plugins/iterm2_profile/iterm2_profile.plugin.zsh
+++ zsh/plugins/iterm2_profile/iterm2_profile.plugin.zsh
@@ -5,29 +5,33 @@
 #####################################################
 
 ###
-# Call this function and pass in a name of an iTerm2 profile
-# (name can contain spaces).
-function iterm2_profile() {
-  # Desired name of profile
-  local profile="$1"
+# This plugin is only relevant if the terminal is iTerm2 on OSX.
+if [[ "$OSTYPE" = darwin* ]] && [[ -n "$ITERM_SESSION_ID" ]] ; then
 
-  # iTerm2 escape code for changing profile
-  local iterm2_code="\x1b]50;SetProfile=$profile\x7"
+  ###
+  # To change the current iTerm2 profile, call this function and pass in a name
+  # of another existing iTerm2 profile (name can contain spaces).
+  function iterm2_profile() {
+    # Desired name of profile
+    local profile="$1"
 
-  # If we're in tmux, a special ecape code must be prepended
-  # so that the iTerm2 escape code is passed on into iTerm2.
-  local prefix=''
-  local suffix=''
-  if [[ -n $TMUX ]]; then
-    prefix='\033Ptmux;\033'
-    suffix='\033\\'
-  fi
+    # iTerm2 escape code for changing profile
+    local iterm2_code="\x1b]50;SetProfile=$profile\x7"
 
-  # send the sequence
-  echo -n "$prefix""$iterm2_code""$suffix"
+    # If we're in tmux, a special ecape code must be prepended
+    # so that the iTerm2 escape code is passed on into iTerm2.
+    local prefix=''
+    local suffix=''
+    if [[ -n $TMUX ]]; then
+      prefix='\033Ptmux;\033'
+      suffix='\033\\'
+    fi
 
-  # update shell variable
-  ITERM_PROFILE="$profile"
-}
+    # send the sequence
+    echo -n "$prefix""$iterm2_code""$suffix"
 
+    # update shell variable
+    ITERM_PROFILE="$profile"
+  }
 
+fi
