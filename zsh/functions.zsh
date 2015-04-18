4fc94aa45011e9b5bd3a318f73ba7c3a3efd987b
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2015-04-18 12:12:19 +0300

    zsh: Functions to change terminal background color
    This uses the new custom iTerm2 profile plugin.

---
+++ zsh/functions.zsh
@@ -0,0 +1,21 @@
+function bg_dark() {
+    iterm2_profile "Solarized Dark"
+    update_tmux_colors
+}
+
+function bg_light() {
+    iterm2_profile "Solarized Light"
+    update_tmux_colors
+}
+
+function update_tmux_colors() {
+    local tmux_file=''
+
+    if [[ "$ITERM_PROFILE" =~ "[Ll]ight" ]]; then
+        tmux_file="$DOTFILES/tmux/airline-colors-light.conf"
+    else
+        tmux_file="$DOTFILES/tmux/airline-colors-dark.conf"
+    fi
+
+    tmux source-file "$tmux_file" > /dev/null
+}
