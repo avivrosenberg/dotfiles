06bdf20aeba6b01342c9dd639687c1e84ca3581d
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2015-07-04 18:53:09 +0300

    zsh: Updated iTerm2 plugin
    Rename and minor fixes.

--- zsh/plugins/iterm2/iterm2.plugin.zsh
+++ zsh/plugins/iterm2/iterm2.plugin.zsh
@@ -1,14 +1,15 @@
 #####################################################
-# iTerm2 Profile changer plugin for oh-my-zsh       #
-# Version: 1.0                                      #
+# iTerm2 plugin for oh-my-zsh                       #
 # Author: Aviv Rosenberg (github.com/avivrosenberg) #
 #####################################################
 
 ###
 # This plugin is only relevant if the terminal is iTerm2 on OSX.
-if [[ "$OSTYPE" = darwin* ]] && [[ -n "$ITERM_SESSION_ID" ]] ; then
+if [[ "$OSTYPE" == darwin* ]] && [[ -n "$ITERM_SESSION_ID" ]] ; then
 
   ###
+  # iterm2_profile(): Function for changing the current terminal window's
+  # profile (colors, fonts, settings, etc).
   # To change the current iTerm2 profile, call this function and pass in a name
   # of another existing iTerm2 profile (name can contain spaces).
   function iterm2_profile() {
@@ -18,7 +19,7 @@ if [[ "$OSTYPE" = darwin* ]] && [[ -n "$ITERM_SESSION_ID" ]] ; then
     # iTerm2 escape code for changing profile
     local iterm2_code="\x1b]50;SetProfile=$profile\x7"
 
-    # If we're in tmux, a special ecape code must be prepended
+    # If we're in tmux, a special escape code must be prepended
     # so that the iTerm2 escape code is passed on into iTerm2.
     local prefix=''
     local suffix=''
@@ -28,7 +29,7 @@ if [[ "$OSTYPE" = darwin* ]] && [[ -n "$ITERM_SESSION_ID" ]] ; then
     fi
 
     # send the sequence
-    echo -n "$prefix""$iterm2_code""$suffix"
+    echo -n "${prefix}${iterm2_code}${suffix}"
 
     # update shell variable
     ITERM_PROFILE="$profile"
