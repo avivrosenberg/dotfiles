3a7163bf88ed2dfaee1a583731e6c4dfe0b1e547
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2018-05-23 12:35:30 +0300

    Create a bash script to generate the symlinks
    The Rakefile requires ruby which isn't always available.

---
+++ make_symlinks.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+DOTFILESDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DESTDIR="$HOME"
+
+EXT=".symlink"
+FILES=($(find $DOTFILESDIR -regex "$DOTFILESDIR/.*$EXT" -not -regex ".*\.git.*"))
+
+source "$DOTFILESDIR/bin$EXT/colorcodes.sh"
+
+echo "${GREEN}DOTFILESDIR: $DOTFILESDIR${RESTORE}"
+echo "${GREEN}DESTDIR: $DESTDIR${RESTORE}"
+
+for filepath in "${FILES[@]}"; do
+
+    # Take only the file name, remove EXT and prepend the DESTDIR
+    filename=$(basename $filepath)
+    target_filename="$DESTDIR/.${filename%${EXT}}"
+
+    # Create the symlink (override previous ones)
+    echo "${BLUE}$filepath${RESTORE} --> ${GREEN}$target_filename${RESTORE}"
+    $(
+    set -x;
+    ln -snFf $filepath $target_filename
+    )
+done
+
