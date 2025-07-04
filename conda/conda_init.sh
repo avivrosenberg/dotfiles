6114c547fc476d0e6d999d73b461204375bacc7d
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2025-07-04 14:08:16 +0300

    Update conda_init.sh

--- conda/conda_init.sh
+++ conda/conda_init.sh
@@ -1,32 +1,26 @@
 
 
 ##
-# Discover conda base dir and shell name 
+# Discover conda base dir and shell name
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    export CONDA_PATH=$(find -E $HOME -maxdepth 1 -type d -regex '[^.]*(miniforge|conda).*' 2>/dev/null | head -n 1)
+    export CONDA_BASE_PATH=$(find -E $HOME -maxdepth 1 -type d -regex '[^.]*(miniforge|conda).*' 2>/dev/null | head -n 1)
 else #if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     # Assume linux if not macos
-    export CONDA_PATH=$(find $HOME -maxdepth 1 -type d -regex '[^.]*\(miniforge\|conda\).*' 2>/dev/null | head -n 1)
+    export CONDA_BASE_PATH=$(find $HOME -maxdepth 1 -type d -regex '[^.]*\(miniforge\|conda\).*' 2>/dev/null | head -n 1)
 fi
-
-__shellname=$(basename $0)
 
 ##
-# Contents below are based on the output of 'mamba init' 
+# Contents below are based on the output of 'mamba init'
 
-__conda_setup="$("$CONDA_PATH/bin/conda" "shell.$__shellname" 'hook' 2> /dev/null)"
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+export MAMBA_EXE="$CONDA_BASE_PATH/bin/mamba";
+export MAMBA_ROOT_PREFIX="$CONDA_BASE_PATH";
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
+    eval "$__mamba_setup"
 else
-    if [ -f "$CONDA_PATH/etc/profile.d/conda.sh" ]; then
-        . "$CONDA_PATH/etc/profile.d/conda.sh"
-    else
-        export PATH="$CONDA_PATH/bin:$PATH"
-    fi
+    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
 fi
-unset __conda_setup
-unset __shellname
-
-if [ -f "$CONDA_PATH/etc/profile.d/mamba.sh" ]; then
-    . "$CONDA_PATH/etc/profile.d/mamba.sh"
-fi
+unset __mamba_setup
+# <<< mamba initialize <<<
