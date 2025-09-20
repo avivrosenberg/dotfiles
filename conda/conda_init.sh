82ef556a226d164cbf64d7d0e4830bcf091ea660
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2025-09-20 12:22:02 +0300

    Update conda_init.sh

--- conda/conda_init.sh
+++ conda/conda_init.sh
@@ -24,3 +24,19 @@ else
 fi
 unset __mamba_setup
 # <<< mamba initialize <<<
+
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$("$CONDA_BASE_PATH/bin/conda " 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "$CONDA_BASE_PATH/etc/profile.d/conda.sh" ]; then
+        . "$CONDA_BASE_PATH/etc/profile.d/conda.sh"
+    else
+        export PATH="$CONDA_BASE_PATH/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
