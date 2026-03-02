3ae38f487691e098ac34a1685804aaa7944ece46
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2026-03-02 18:06:22 +0200

    nvim: update configs

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,8 +4,14 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
-map("i", "<C-h>", "<BS>", { desc = "Backspace" })
+-- Disable arrow keys in normal mode
+map("n", "<Down>", "<NOP>")
+map("n", "<Up>", "<NOP>")
+map("n", "<Left>", "<NOP>")
+map("n", "<Right>", "<NOP>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Insert mode navigation/deletion
+map("i", "<C-l>", "<Del>", { desc = "Delete forward" })
+map("i", "<C-h>", "<BS>", { desc = "Backspace" })
+map("i", "<C-b>", "<Left>", { desc = "Move left" })
+map("i", "<C-f>", "<Right>", { desc = "Move right" })
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,37 @@
 require "nvchad.options"
 
--- add yours here!
+local o = vim.opt
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+-- Leader is already Space in NvChad, but you used "," -- decide if you want to change it
+-- vim.g.mapleader = ","  -- uncomment to restore your old leader
+
+-- Editing behavior
+o.wrap = false          -- don't wrap lines (vimrc: nowrap)
+o.linebreak = true      -- wrap at convenient points if wrap is on
+o.scrolloff = 2         -- keep lines visible at top/bottom
+o.timeoutlen = 500      -- faster chord timeout (matches your vimrc)
+o.foldlevelstart = 99   -- open all folds on file open
+o.conceallevel = 0      -- disable conceal globally
+o.spelllang = "en_us"
+o.clipboard = "unnamed" -- yank to system clipboard
+
+-- Searching
+o.ignorecase = true
+o.smartcase = true
+
+-- Indentation defaults (4 spaces, like your vimrc)
+o.shiftwidth = 4
+o.softtabstop = 4
+o.tabstop = 4
+o.expandtab = true
+o.shiftround = true
+
+-- Diff
+o.diffopt:append("vertical")
+
+-- Backup
+o.backup = true
+o.backupdir = vim.fn.expand("~/.vimtmp")
+o.backupext = ".vimbackup"
+o.swapfile = false
+vim.fn.mkdir(vim.fn.expand("~/.vimtmp"), "p")
