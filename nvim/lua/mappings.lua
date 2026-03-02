55829de3c5bd7fef08f2e56a33a5f589d922d7eb
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2026-03-02 17:37:40 +0200

    nvim: update mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "<C-h>", "<BS>", { desc = "Backspace" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
