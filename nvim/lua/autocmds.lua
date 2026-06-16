cde43110a2dc230ece4dc54cb686e193b705fd9f
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2026-06-16 08:27:00 +0300

    nvim: markdown settings

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+-- Configure settings for markdown files
+local md = vim.api.nvim_create_augroup("md_settings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = md,
+    pattern = "markdown",
+    -- textwidth, wrap guide, auto-wrap if over width
+    command = "setlocal textwidth=88 colorcolumn=89 formatoptions+=t",
+})
