e296b45a0e0c713dd351654072a576342721b105
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2026-06-18 14:59:00 +0300

    nvim: update plugings

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -33,4 +33,15 @@ return {
             },
         },
     },
+
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            require("telescope").load_extension("file_browser")
+        end,
+    }
 }
