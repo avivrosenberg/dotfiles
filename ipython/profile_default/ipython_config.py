8f49ed05265c0c46929d12cb5244d6f0574b7e54
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2026-06-21 10:01:54 +0300

    Update ipython_config.py

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,5 +1,15 @@
 c = get_config()
+
 c.TerminalInteractiveShell.confirm_exit = False
 
-c.InteractiveShellApp.extensions = ['autoreload']  
-c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+c.InteractiveShellApp.extensions = ["autoreload"]
+c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+
+# Keyboard shortcuts
+c.TerminalInteractiveShell.shortcuts = [
+    {
+        "new_keys": ["c-g"],
+        "command": "IPython:shortcuts.open_input_in_editor",
+        "create": True,
+    }
+]
